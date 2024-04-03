45c43c85075c3f19d24a88fe5c2259e4cd5e1359
Touhid Mia <touhid@banglafighter.com>
2024-04-04 01:54:11 +0600

    Working on uWsgi Server

--- application/pweb-basic/pweb_basic/pweb_basic_module.py
+++ application/pweb-basic/pweb_basic/pweb_basic_module.py
@@ -26,8 +26,6 @@ class PWebBasicModule(PWebComponentRegister):
 
     def run_on_start(self, pweb_app, config):
         print("run_on_start Environment : " + os.environ.get('ENV', 'Local ENV'))
-        print("run_on_start Environment : " + os.environ.get('PWEB_ENV', 'pweb_env'))
-        print(os.environ)
         PWebAuthRegistry.add_start_with_url_in_skip("/pweb-socket")
         if pweb_app.is_app_loaded():
             print("Call Once during app load")
